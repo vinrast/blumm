bb34e5467cf96207eba95aae313265b8daed2db9
vinrast <vinrast@gmail.com>
2024-04-12 17:18:21 -0300

    refactor: add port value

--- server/src/shared/config/app.config.ts
+++ server/src/shared/config/app.config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-const { SERVER_PORT } = dotenv.config().parsed;
+const { SERVER_PORT = 8080 } = dotenv.config().parsed;
 
 export const APP_CONFIG = {
   SERVER_PORT: +SERVER_PORT,
